8bea5bf53110bee8e519c47a40054657e6c1b79c
Aayushi <aayushikukreja@gmail.com>
2023-08-11 20:50:48 +0530

    Modified some files

--- cms/src/components/ResearchPaper/Research.js
+++ cms/src/components/ResearchPaper/Research.js
@@ -150,7 +150,7 @@ const Research = () => {
               name="submit"
               value="submit"
             >
-              Submit
+              Submit Record
             </button>
             <button
               className="resetButton"
